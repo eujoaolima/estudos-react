d8efc1270cc77c81bdc2bbc65e56a07cb75db5d8
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-03-13 12:03:05 -0300

    Commit 2.1.1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(rootElement);
+
+root.render("Hello World!");
+
+let nome = "Batata";
+
+
+
+// JSX = Mescla do HTML com Javascript
+
+root.render( // Se não colocar uma tag section não é possível colocar várias informações dentro da função render
+    <section>
+        <h1>Fireball</h1>
+
+        <p>
+            Isso aqui é um texto escrito em <br /> <b>JSX</b>
+        </p>
+
+        <p>
+            Estou puxando variáveis com o React, o valor da variável é {nome.toUpperCase()};
+        </p>
+    </section>
+
+); // Como a variável root foi chamada 2 vezes, o JSX lê a alteração mais atual
